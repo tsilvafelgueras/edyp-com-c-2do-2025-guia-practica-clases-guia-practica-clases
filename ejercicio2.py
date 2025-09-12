5339b4ffe8fa7e745e56523b5e311f708d6f4031
tsilvafelgueras <tsilvafelgueras@itba.edu.ar>
2025-09-12 10:54:52 -0300

    ejercicio2

--- ejercicio2.py
+++ ejercicio2.py
@@ -14,24 +14,9 @@
 """
 En este ejercicio vamos a modelar una computadora, creando la clase `Computadora` para ello.
 
-Primero, pensá un rato en todas las características que mirarías al momento de comprar una. ¿Cómo llamamos a esas características en el paradigma de la programación orientada a objetos?
-
-Luego de pensarlo, continuá con la consigna.
-
-- El objeto computadora debe ser instanciado con todos sus atributos pasados como parámetros al método constructor. Al momento de crear el equipo, asignales a los atributos valores por defecto. ¿Qué criterio tuviste en cuenta para elegir esos valores?
-
 - El método `__str__` nos ayuda a conocer la información esencial de nuestros objetos. ¡Implementalo! Recordá que siempre debería estar presente en las clases que creás.
 
 - Instanciá 3 computadoras y asignales distintos valores a sus atributos.
-
-- ¿Cómo podrías llevar la cuenta de la cantidad de computadoras creadas? ¿Qué tipo de variable resuelve lo pedido?
-
-- Implementá al menos 2 de los siguientes métodos en la clase Computadora:
-
-  - `updateOS`: Actualiza el sistema operativo.
-  - `PM`: Brinda un mantenimiento programado al hardware del equipo.
-  - `addRAM`: Instala un nuevo módulo de RAM en la computadora.
-  - `getCapacity`: Muestra la capacidad del componente de hardware que se desee conocer.
 
 > Nota: Pedile ayuda a tu monitor para pensar cómo implementar los métodos solicitados. Pensá en aquellos atributos creados u otros adicionales que les den funcionalidad y practicidad a dichos métodos. Si es necesario, podés investigar el uso de `datetime` u otra librería que requieras. Recordá informar siempre al usuario sobre las acciones realizadas en el sistema. Probá todos los métodos creados para testear su funcionamiento.
 
@@ -41,27 +26,64 @@ Luego de pensarlo, continuá con la consigna.
 
 class Computadora:
     conteoComputadora = 0
-    posiblesSistOperativos = ['Windows 11', 'iOs 18']
+    posiblesSistOperativos = ['Windows 11', 'macOs']
 
-    def __init__(self, marca:str, modelo:str, memoriaRAM:int, almacenamiento: int, procesador: str, esta_prendida = False, sistemaOperativo = 'Windows 11'):
+    def __init__(self, marca:str, modelo:str, memoriaRAM:int, almacenamiento: int, procesador: str, sistemaOperativo = 'Windows 11'):
         self.marca = self.validar_cadena(marca)
         self.modelo = self.validar_cadena(modelo)
         self.memoriaRAM = memoriaRAM
         self.almacenamiento = almacenamiento
         self.procesador = self.validar_cadena(procesador)
-        self.esta_prendida = esta_prendida
         self.sistemaOperativo = self.validar_cadena(sistemaOperativo)
-
-        def validar_cadena(self,cadena):
-            if not isinstance(cadena, str):
-                raise TypeError(f"{cadena} debe ser una cadena de texto")
-            if len(cadena)<1:
-                raise ValueError(f"{cadena} no puede ser una cadena vacía")
-            return cadena
-    
         Computadora.conteoComputadora += 1
+
+    def __str__(self):
+        return f'Computadora: {self.marca}, {self.modelo} (Almacenamiento: {self.almacenamiento}, RAM: {self.memoriaRAM}, CPU: {self.procesador}, OS: {self.sistemaOperativo})'
+    
+    def validar_cadena(self,cadena):
+        if not isinstance(cadena, str):
+            raise TypeError(f"{cadena} debe ser una cadena de texto")
+        if len(cadena)<1:
+            raise ValueError(f"{cadena} no puede ser una cadena vacía")
+        return cadena
+
+    def updateOS(self, nueva_version):
+        self.sistemaOperativo = nueva_version
+        print(f"Sistema operativo actualizado a {self.sistemaOperativo}")
+
+    def addRAM(self, cantidad_gb):
+        if cantidad_gb > 0:
+            self.memoriaRAM += cantidad_gb
+            print(f'Se ha instalado {cantidad_gb}GB de RAM. Memoria actualizada: {self.memoriaRAM}')
+        else:
+            print("Error: la cantidad de RAM debe ser un número positivo") 
+
+    def getter_capacity(self):
+        print(f"La capacidad de almacenamietno es: {self.almacenamiento} gb")
+        return self.almacenamiento
+    
 
     @classmethod
     def mostrar_conteo (cls):
         print(f'El número de computadoras es {cls.conteoComputadora}')
 
+
+if __name__ == '__main__':
+    try:
+        lenovo1 = Computadora("Lenovo", "ThinkPad T14s", 16, 250, "Intel i8")
+        mac1 = Computadora("Apple", "Mac Air", 16, 512, "Apple Silicon", "macOs")
+        dell1 = Computadora("Dell", "Dell 123", 8, 256, "Intel i9")
+
+        print(lenovo1)
+        print(mac1)
+
+        lenovo1.addRAM(8)
+        mac1.updateOS("Mac Sonoma")
+        dell1.getter_capacity()
+
+    except ValueError as e:
+        print("El error es", e)
+    except TypeError as e:
+        print("El error es", e)
+    except Exception as e:
+        print("El error es", e)
